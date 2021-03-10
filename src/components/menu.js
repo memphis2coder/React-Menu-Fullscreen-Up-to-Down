74b7d9b36e7d40247c3d26638630f247271ffd39
Antonio <memphis2coder@gmail.com>
2021-03-10 17:57:22 -0500

    completed menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react'
+ import React, {useEffect, useRef} from 'react'
 import {Link} from 'react-router-dom';
 import { gsap } from 'gsap';
 import "../globalStyles.css";
@@ -7,42 +7,67 @@ import './menu.css';
 const Menu = ({state}) => {
     // reference to the DOM node
     let menu = useRef(null);
+
     // toggle menu open and close
     useEffect(() => {
          if (state.clicked === false) { // if its false menu is closed
-            menu.style.display = 'none';
+            gsap.to(menu, {
+                duration: 0.8,
+                height: 0,
+                ease: "power3.inOut"
+            });
+            gsap.to(menu, {
+                duration: 1,
+                css: {display: "none"}
+            });
             console.log('close menu');
-         } else if (state.clicked === true || (state.clicked === true && state.initial === null)) {
-            menu.style.display = 'flex'; // if its true open menu
+        } else if (state.clicked === true || 
+            (state.clicked === true && state.initial === null)) 
+            { // if its true open menu
+            gsap.to(menu, {
+                css: {display: "flex"},
+            });
+            gsap.to(menu, {
+                duration: 0,
+                height: "100%",
+            });
+            gsap.from(menu, {
+                height: 0,
+                duration: 1,
+                ease: "power3.inOut"
+            })
             console.log("open menu");
-         }
+        }
     },[state]);
+
     // reference to the animation
-    let myTween = useRef(null)
+    
     
     
 
     return (
-        <div ref={el => (menu = el)} className="main-menu">
-            <div className="container">
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/team">Team</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div> 
+        <div ref={el => (menu = el)} className="menu">
+            <div className ="menu-layer">
+                <div className="container">
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/team">Team</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div> 
+            </div>
         </div>
     )
 }
 
-export default Menu
+export default Menu;
